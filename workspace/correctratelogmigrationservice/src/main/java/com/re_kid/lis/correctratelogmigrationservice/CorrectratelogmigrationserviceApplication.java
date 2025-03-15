f6bac306d4a5724780d65dc705efb8ece9221458
Lis0715 <s.yamamoto.ad98@gmail.com>
2025-03-15 14:33:23 +0900

    プロジェクト作成

---
+++ workspace/correctratelogmigrationservice/src/main/java/com/re_kid/lis/correctratelogmigrationservice/CorrectratelogmigrationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.re_kid.lis.correctratelogmigrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectratelogmigrationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectratelogmigrationserviceApplication.class, args);
+	}
+
+}
---
+++ workspace/correctratelogmigrationservice/src/test/java/com/re_kid/lis/correctratelogmigrationservice/CorrectratelogmigrationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re_kid.lis.correctratelogmigrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectratelogmigrationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
