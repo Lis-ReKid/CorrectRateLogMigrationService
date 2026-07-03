c32ebffbd8a399c3c77753ce2a947a105ae90ac1
Lis0715 <s.yamamoto.ad98@gmail.com>
2026-07-03 15:02:30 +0900

    bodyを文字列のみ（現行と同じ仕様）に修正

--- functions/issue_migration_id/issue_migration_id.py
+++ functions/issue_migration_id/issue_migration_id.py
@@ -1,5 +1,4 @@
 import logging
-import json
 
 import migration_id_service
 
@@ -10,7 +9,5 @@ def handler(event, context):
     migration_id = migration_id_service.execute()
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": migration_id,
-        }),
+        "body": migration_id,
     }
