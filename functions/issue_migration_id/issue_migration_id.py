7dc888fb2b308e625c5a62f2ef74990ade8918cd
Lis0715 <s.yamamoto.ad98@gmail.com>
2026-07-03 14:36:52 +0900

    移行ID（ランダム文字列）を返戻

--- functions/issue_migration_id/issue_migration_id.py
+++ functions/issue_migration_id/issue_migration_id.py
@@ -1,8 +1,16 @@
 import logging
+import json
+
 import migration_id_service
 
 logging.getLogger().setLevel(logging.INFO)
 
 
 def handler(event, context):
-    migration_id_service.issue_migration_id()
+    migration_id = migration_id_service.execute()
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": migration_id,
+        }),
+    }
--- functions/issue_migration_id/migration_id_service.py
+++ functions/issue_migration_id/migration_id_service.py
@@ -1,7 +1,18 @@
 import logging
+import random
 
 logger = logging.getLogger(__name__)
 
 
-def issue_migration_id():
+def execute():
     logger.info('Start issue migration id.')
+
+    # 移行IDを返却
+    logger.info('migration ID issued.')
+    return get_migration_id()
+
+
+def get_migration_id():
+    # 文字セットの中からサイズ8のランダム文字列を返す。
+    allowed_chars = "abcdefghijkmnopqrstuvwxyz123456789"
+    return "".join(random.choices(allowed_chars, k=8))
